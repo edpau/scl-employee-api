e3f623d2297cad787948ae22322f4e9c7cb864c8
edpau <edpau@me.com>
2025-07-29 12:54:42 +0100

    feat(domain): implement EmployeeRepository with findAll method

---
+++ app/employee/EmployeeRepository.scala
@@ -0,0 +1,19 @@
+package employee
+
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import slick.jdbc.MySQLProfile.api._
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class EmployeeRepository @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+  import dbConfig._
+  import employee.EmployeeTable.employees
+
+  def findAll(): Future[Seq[Employee]] = {
+    db.run(employees.result)
+  }
+}
