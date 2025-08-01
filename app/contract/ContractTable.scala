cb2ba68fc5f044a34cb0fd450eaa4ef14e165892
edpau <edpau@me.com>
2025-08-01 15:44:22 +0100

    feat(contract): create Contracts Slick table with foreign key to Employees

---
+++ app/contract/ContractTable.scala
@@ -0,0 +1,50 @@
+package contract
+
+import employee.Employees
+import slick.jdbc.MySQLProfile.api._
+import java.sql.Timestamp
+import java.time.LocalDate
+
+class Contracts(tag: Tag) extends Table[Contract](tag, "contracts") {
+
+  def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+
+  def employeeId = column[Int]("employee_id")
+
+  def contractType = column[String]("contract_type")
+
+  def employmentType = column[String]("employment_type")
+
+  def startDate = column[LocalDate]("start_date")
+
+  def endDate = column[Option[LocalDate]]("end_date")
+
+  def hoursPerWeek = column[Int]("hours_per_week")
+
+  def createdAt = column[Option[Timestamp]]("created_at")
+
+  def updatedAt = column[Option[Timestamp]]("updated_at")
+
+  def contractFk = foreignKey(
+    "fk_employee",
+    employeeId,
+    TableQuery[Employees]
+  )(_.id, onDelete = ForeignKeyAction.SetNull)
+
+  def * = (
+    id.?,
+    employeeId,
+    contractType,
+    employmentType,
+    startDate,
+    endDate,
+    hoursPerWeek,
+    createdAt,
+    updatedAt
+  ) <> ((Contract.apply _).tupled, Contract.unapply)
+
+}
+
+object ContractTable {
+  val contracts = TableQuery[Contracts]
+}
