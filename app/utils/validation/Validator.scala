259f503ff3ba2c424062d4f32b840382ea4ec0a8
edpau <edpau@me.com>
2025-08-04 13:00:51 +0100

    feat(validation): add support for optional int and date validation
    - Added `isPositiveIfDefined` for optional Int validation
    - Added `isDateRangeValidIfDefined` for optional LocalDate range checks

--- app/utils/validation/Validator.scala
+++ app/utils/validation/Validator.scala
@@ -17,6 +17,12 @@ trait Validator {
     if (value <= 0) Some(fieldName -> s"$fieldName must be greater than 0")
     else None
 
+  def isPositiveIfDefined(fieldName: String, value: Option[Int]): Option[(String, String)] =
+    value match {
+      case Some(v) if (v <= 0) => Some(fieldName -> s"$fieldName must be greater than 0")
+      case _ => None
+    }
+
   def isDateRangeValid(startDate: LocalDate, endDate: Option[LocalDate]): Option[(String, String)] = {
     endDate match {
       case Some(end) if !startDate.isBefore(end) =>
@@ -24,5 +30,13 @@ trait Validator {
       case _ => None
     }
   }
+
+    def isDateRangeValidIfDefined(startDate: Option[LocalDate], endDate: Option[LocalDate]): Option[(String, String)] = {
+      (startDate, endDate) match {
+        case (Some(start), Some(end)) if !start.isBefore(end) =>
+          Some("endDate" -> "endDate must be after startDate")
+        case _ => None
+      }
+    }
 
 }
